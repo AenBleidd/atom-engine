54736d3b6e90a31b4fc38db453dd2835bf4fa7a4
L Lawliet <lestat.de.lionkur@gmail.com>
2010-02-23 13:08:48 +0200

    CORE: ?????????? ??????? ?????????? ?????????? ?????? ?????? ???????. FSMAN: ?????????? ??????? ??????? ???????????? ?????? ????????? ??? unix ? ???????? ??? windows. ?????????? ?????????? makefile

---
+++ Makefile
@@ -0,0 +1,49 @@
+CXX = g++
+FLAGS := -Wall -c
+#For debug
+ifeq ($(DEBUG),TRUE)
+	FLAGS += $(FLAGS) -g -DATOM_DEBUG
+endif
+#For linux
+ifeq ($(SYSTEM),UNIX)
+	FLAGS += $(FLAGS) -DUNIX
+endif
+COREPATH = core/obj/
+COREOBJ = $(COREPATH)error.o $(COREPATH)gamefs.o $(COREPATH)window.o $(COREPATH)main.o
+CORELIB = -lSDL -lGL
+UTILSPATH = utils/obj/
+FSMANFLAGS = -D_FSMANAGER_
+FSMANOBJ = $(UTILSPATH)crc32.o $(UTILSPATH)error.o $(UTILSPATH)gamefs.o $(UTILSPATH)fsman.o 
+
+all : atom fsman
+.PHONY : all
+
+# Atom Engine
+atom : $(COREOBJ)
+	-mkdir atom-engine-test
+	$(CXX) $(CORELIB) $(COREOBJ) -o atom-engine-test/atom
+$(COREPATH)error.o : core/error.cpp core/error.h preproc.h
+	$(CXX) $(FLAGS) core/error.cpp -o $(COREPATH)error.o
+$(COREPATH)gamefs.o : core/gamefs.cpp core/gamefs.h preproc.h core/error.h
+	$(CXX) $(FLAGS) core/gamefs.cpp -o $(COREPATH)gamefs.o
+$(COREPATH)window.o : core/window.cpp core/window.h core/error.h
+	$(CXX) $(FLAGS) core/window.cpp -o $(COREPATH)window.o
+$(COREPATH)main.o : core/main.cpp preproc.h core/window.h core/error.h
+	$(CXX) $(FLAGS) core/main.cpp -o $(COREPATH)main.o
+
+# File System Manager (FSMan)
+fsman: $(FSMANOBJ)
+	-mkdir atom-engine-test
+	$(CXX) $(FSMANOBJ) -o atom-engine-test/fsman
+$(UTILSPATH)crc32.o : utils/crc32.cpp utils/crc32.h
+	$(CXX) $(FLAGS) $(FSMANFLAGS) utils/crc32.cpp -o $(UTILSPATH)crc32.o
+$(UTILSPATH)error.o : core/error.cpp core/error.h preproc.h
+	$(CXX) $(FLAGS) $(FSMANFLAGS) core/error.cpp -o $(UTILSPATH)error.o
+$(UTILSPATH)gamefs.o : core/gamefs.cpp core/gamefs.h preproc.h core/error.h utils/crc32.h
+	$(CXX) $(FLAGS) $(FSMANFLAGS) core/gamefs.cpp -o $(UTILSPATH)gamefs.o
+$(UTILSPATH)fsman.o : utils/fsman.cpp preproc.h core/gamefs.h core/error.h
+	$(CXX) $(FLAGS) $(FSMANFLAGS) utils/fsman.cpp -o $(UTILSPATH)fsman.o
+
+.PHONY : clean
+clean : 
+	-rm $(COREOBJ) $(FSMANOBJ) atom-engine-test/atom atom-engine-test/fsman
--- core/gamefs.cpp
+++ core/gamefs.cpp
@@ -1,5 +1,8 @@
 #include "gamefs.h"
+
 AtomFS::AtomFS() {
+// Generate crypt key
+  GenKey(0xCEF2F7E0, 0xFFEDE8E5, 0x20C2E0EC, 0xEFE8F0E0);
 // create root directory
   root = new TREE_FOLDER;
   char name[] = "/\0";
@@ -20,6 +23,92 @@ AtomFS::AtomFS() {
 AtomFS::~AtomFS() {
 // delete root directory
   delete root;
+}
+// Table generation
+void AtomFS::GenKey(unsigned int k0, unsigned int k1, unsigned int k2, unsigned int k3)
+{
+  long x, z;
+  int p ;
+  static long tt[10]= {
+                       0x726a8f3bL, // table
+                       0xe69a3b5cL,
+                       0xd3c71fe5L,
+                       0xab3c73d2L,
+                       0x4d3a8eb3L,
+                       0x0396d6e8L,
+                       0x3d4c2f7aL,
+                       0x9ee27cf3L, } ;
+  wake_table[0] = k0;
+  wake_table[1] = k1;
+  wake_table[2] = k2;
+  wake_table[3] = k3;
+  for (p=4 ; p<256 ; p++)
+  {
+    x=wake_table[p-4]+wake_table[p-1] ;  // fill t
+    wake_table[p]= (x>>3) ^ tt[(unsigned char)(x&7)] ;
+  }
+  for (p=0 ; p<23 ; p++)
+    wake_table[p]+=wake_table[p+89] ; 		  // mix first entries
+  x=wake_table[33] ; z=wake_table[59] | 0x01000001L ;
+  z=z&0xff7fffffL ;
+  for (p=0 ; p<256 ; p++) {//change top byte to
+    x=(x&0xff7fffffL)+z ;  // a permutation etc
+    wake_table[p]=(wake_table[p] & 0x00ffffffL) ^ x ;
+  }
+  wake_table[256]=wake_table[0] ;
+  unsigned char y=(unsigned char)(x);
+  for (p=0 ; p<256 ; p++) { // further change perm.
+    wake_table[p]=wake_table[y=(unsigned char)(wake_table[p^y]^y)];  // and other digits
+    wake_table[y]=wake_table[p+1] ;
+  }
+}
+#ifdef _FSMAN_
+void AtomFS::Crypt(unsigned int *data, int lenght, unsigned int k[4], unsigned int r[4], unsigned int *t)
+{
+  int d;
+  unsigned int r1,r2,r3,r4,r5,r6, *e,m=0x00ffffff ;
+  r3=k[0] ; r4=k[1] ; r5=k[2] ; r6=k[3] ;
+  if (lenght<0) d= -1 ; else d=1 ;
+  e=data+lenght ;
+  while (data-e) {
+    r1 = *data ;
+    r2 = r1^r6 ;
+    *data = r2 ; // Change into r1 for decoding.
+    data += d ;
+    r3 = r3+r2 ;
+    r3 = (r3>>8&m)^wake_table[r3&255] ;
+    r4 = r4+r3 ;
+    r4 = (r4>>8&m)^wake_table[r4&255] ;
+    r5 = r5+r4 ;
+    r5 = (r5>>8&m)^wake_table[r5&255] ;
+    r6 = r6+r5 ;
+    r6 = (r6>>8&m)^wake_table[r6&255] ;
+  }
+  r[0] =r3 ; r[1]=r4 ; r[2]=r5 ; r[3]=r6 ;
+}
+#endif // _FSMAN_
+void AtomFS::Decrypt (unsigned int *data, int lenght, unsigned int k[4], unsigned int r[4], unsigned int *t)
+{
+  int d;
+  unsigned int r1,r2,r3,r4,r5,r6, *e,m=0x00ffffff ;
+  r3=k[0] ; r4=k[1] ; r5=k[2] ; r6=k[3] ;
+  if (lenght<0) d= -1 ; else d=1 ;
+  e=data+lenght ;
+  while (data-e) {
+    r1 = *data ;
+    r2 = r1^r6 ;
+    *data = r2 ;
+    data += d ;
+    r3 = r3+r1 ;
+    r3 = (r3>>8&m)^wake_table[r3&255] ;
+    r4 = r4+r3 ;
+    r4 = (r4>>8&m)^wake_table[r4&255] ;
+    r5 = r5+r4 ;
+    r5 = (r5>>8&m)^wake_table[r5&255] ;
+    r6 = r6+r5 ;
+    r6 = (r6>>8&m)^wake_table[r6&255] ;
+  }
+    r[0] =r3 ; r[1]=r4 ; r[2]=r5 ; r[3]=r6 ;
 }
 int AtomFS::Mount (char* filename) {
   atomlog.DebugMessage("Begin mounting filesystem...");
@@ -161,3 +250,156 @@ int AtomFS::Mount (char* filename, char* mountfolder, unsigned char priority) {
   atomlog.DebugMessage(pbuf);
   return 0;
 }
+// Remove folder links
+#ifdef _FSMAN_
+static int dot_exclude (const struct dirent64 *dir)
+{
+  if ((dir->d_name[0] == '.') && (dir->d_name[1] == 0)) return 0;
+  if ((dir->d_name[0] == '.') && (dir->d_name[1] == '.')&& (dir->d_name[2] == 0)) return 0;
+  return 1;
+}
+long int AtomFS::FolderScan(char *ch,int level=0)
+{
+  long int res=0;
+  if (ch !=NULL)
+  {
+    if (chdir(ch) !=0) return res;
+    struct dirent64 **eps;
+    struct stat64 st;
+    int n;
+    n = scandir64 ("./", &eps, dot_exclude, alphasort64);
+    if (n >= 0)
+    {
+      int cnt;
+      for (cnt = 0; cnt < n; ++cnt)
+      {
+        int i=0;
+        while (i<(level-1))
+        {
+          // printf (" | ");
+
+          atomlog.DebugMessage(" | ");
+
+          i++;
+        }
+        if (eps[cnt]->d_type == DT_DIR)
+        {
+          // printf("%-30s\n",eps[cnt]->d_name);
+
+          char buf[1000];
+          sprintf(buf, "%-30s\n", eps[cnt]->d_name);
+          atomlog.DebugMessage(buf);
+
+          long int size=FolderScan(eps[cnt]->d_name, level+1);
+          res+=size;
+        }
+        if (eps[cnt]->d_type==DT_REG)
+        {
+          if (stat64(eps[cnt]->d_name, &st) == 0) res+=(long int)st.st_size;
+
+          char buf[1000];
+          sprintf(buf, "%-30s   %ld\n", eps[cnt]->d_name, (long int)st.st_size);
+          atomlog.DebugMessage(buf);
+
+          // printf("%-30s   %ld\n",eps[cnt]->d_name, (long int)st.st_size);
+        }
+      }
+    for (cnt = 0; cnt < n; ++cnt) free(eps[cnt]);
+    free(eps);
+    }
+    else
+    atomlog.LogMessage("Couldn't open the directory");
+  chdir("..");
+  }
+  return res;
+}
+#endif // _FSMAN_
+/*
+#include <cstdlib>
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+
+using namespace std;
+
+bool DEBUG = FALSE;
+char folder[2048];
+char path[2048];
+char size[5];
+int nsize = 1024;
+char tg[3] = {"Kb"};
+int filecount = 0;
+char foldername[1024];
+char filename[3072];
+
+struct LIST
+{
+       int number;
+       char folder[3072];
+       char path[3072];
+       LIST *link;
+};
+
+struct STRING
+{
+       char folder[3072];
+       char path[3072];
+};
+
+LIST *head, *list, *prev;
+void ScanDir()
+{
+    WIN32_FIND_DATA FindFileData;
+	HANDLE hf;
+	int n = 0;
+	char file[3072], temp[3072], wildcard[3072];
+	strcpy(path, folder);
+	strcat(path, "\\*");
+
+	hf=FindFirstFile(path, &FindFileData);
+	if (hf!=INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+            strcpy(path, folder);
+            strcat(path, "\\*");
+            if(FindFileData.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY && strcmp(FindFileData.cFileName, ".") != 0 && strcmp(FindFileData.cFileName, "..") != 0)
+            {
+             strcpy(temp, folder);
+             strcat(temp, "\\");
+			 strcpy(foldername, FindFileData.cFileName);
+			 strcat(temp, foldername);
+			 if (DEBUG == true)
+			  cout << "folder: " << temp << "\n";
+			 HANDLE handle;
+			 strcpy(wildcard, temp);
+			 strcat(wildcard, "\\*.txt");
+			 handle=FindFirstFile(wildcard, &FindFileData);
+			 if (handle!=INVALID_HANDLE_VALUE)
+			 {
+               do
+               {
+                 strcpy(file, temp);
+                 strcat(file, "\\");
+                 strcat(file, FindFileData.cFileName);
+                 if (DEBUG == true)
+			        cout << "file: " << file << "\n";
+                 list = new LIST;
+                 strcpy(list->folder, foldername);
+                 strcpy(list->path, file);
+                 list->number = filecount;
+                 if (prev != NULL) prev->link = list;
+			     if (head == NULL) head = list;
+			     prev = list;
+                 filecount++;
+               }
+             while(FindNextFile(handle, &FindFileData)!=0);
+             FindClose(handle);
+             }
+            }
+		}
+		while (FindNextFile(hf,&FindFileData)!=0);
+		FindClose(hf);
+	}
+}
+*/
