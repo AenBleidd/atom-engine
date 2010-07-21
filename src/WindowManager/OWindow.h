c42bcad90385df72d733856f9365dec0739c64b4
Alexandr <alssandro2007@gmail.com>
2010-07-22 00:13:43 +0300

    Tiny improvements in AtomError module.

--- src/WindowManager/OWindow.h
+++ src/WindowManager/OWindow.h
@@ -2,12 +2,40 @@
 #define _OWINDOW_H
 #include <AtomError.h>
 
+enum VISIBILITY
+{
+	visible,
+	hidden
+};
+
+enum EFFECT
+{
+	normal,
+	minimized,
+	maximized,
+	fullscreen
+};
+
+enum BORDER
+{
+	noborder,
+	sizeable,
+	fixed
+};
+
 class OWindow
 {
 public:
 	OWindow( AtomLog* );
-	virtual bool MakeWindow( void ) = 0;
-	virtual void ResizeWindow( int, int ) = 0;
+	struct properties
+	{
+		unsigned char title[0xFF];
+		int x, y, width, height;
+		VISIBILITY visibility;
+		EFFECT effect;
+		BORDER border;
+	} properties;	
+	virtual bool Create( void ) = 0;
 protected:
 	AtomLog *log;
 };
