8220cca4ea93c24e77ab1fd90162a451a17278b6
L Lawliet <lestat.de.lionkur@gmail.com>
2010-12-26 01:57:30 +0200

    ?????????? ?????????? ? ????? ? ????????????? ????????

--- src/GameFS/crc32.h
+++ src/GameFS/crc32.h
@@ -1,5 +1,6 @@
 #ifndef _CRC32_H_
 #define _CRC32_H_
+#include <inttypes.h>
 // Function hashing crc32
-unsigned long int GenCRC32(unsigned char *buf, unsigned long int lenght);
+uint32_t GenCRC32(uint8_t *buf, uint64_t lenght, uint32_t crc = 0);
 #endif
