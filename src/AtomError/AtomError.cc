24831e997f58ec47b49e7ed9b50ff0cb02a075b6
L Lawliet <lestat.de.lionkur@gmail.com>
2011-07-25 11:22:32 +0300

    ????????? ? ???????? ?????? ??? ?? ? ??? ????????? ?????? ? ?? ????????.

--- src/AtomError/AtomError.cc
+++ src/AtomError/AtomError.cc
@@ -199,12 +199,44 @@ void AtomLog::SetLastError(uint32_t code, uint32_t subcode,
   }
   global_error.code = code;
   global_error.sub_code = subcode;
-  uint32_t errlen = 300 + strlen(errorcode[global_error.code]) +
-               strlen(errorsubcode[global_error.code][global_error.sub_code]);
+// SIC! This is last system error. It may be unusefull in some situations.
+#ifdef WINDOWS
+  global_error.system_code = GetLastError();
+#endif  // WINDOWS
+#ifdef UNIX
+  global_error.system_code = errno;
+#endif  // UNIX
+  uint32_t errlen = strlen(errorcode[global_error.code]) +
+             strlen(errorsubcode[global_error.code][global_error.sub_code]);
   global_error.description = new char[errlen];
-  snprintf(global_error.description, errlen, "%s:%i\tERROR: %s\t%s",
-           file, line, errorcode[global_error.code],
-           errorsubcode[global_error.code][global_error.sub_code]);
+// TODO (Lawliet): Rewrite this
+  if (verbose_level == SHOWSYSTEMERRORSDESCR) {
+    errlen += 300 + 30 + MSG_BUFFER_SIZE;
+#ifdef WINDOWS
+    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                  NULL, global_error.system_code,
+                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), MsgBuf,
+                  MSG_BUFFER_SIZE, NULL);
+#endif  // WINDOWS
+#ifdef UNIX
+    strerror_r(global_error.system_code, MsgBuf, MSG_BUFFER_SIZE);
+#endif  // UNIX
+    snprintf(global_error.description, errlen, "%s:%i\tERROR: %s\t%s\n%s %x: %s",
+             file, line, errorcode[global_error.code],
+             errorsubcode[global_error.code][global_error.sub_code],
+             "Last system error is", global_error.system_code, MsgBuf);
+  } else if (verbose_level == SHOWSYSTEMERRORS) {
+    errlen += 300 + 30;
+    snprintf(global_error.description, errlen, "%s:%i\tERROR: %s\t%s\n%s %x",
+             file, line, errorcode[global_error.code],
+             errorsubcode[global_error.code][global_error.sub_code],
+             "Last system error is:", global_error.system_code);
+  } else {
+    errlen += 300;
+    snprintf(global_error.description, errlen, "%s:%i\tERROR: %s\t%s",
+             file, line, errorcode[global_error.code],
+             errorsubcode[global_error.code][global_error.sub_code]);
+  }
 // log the error
   LogMsg(global_error.description);
 }
