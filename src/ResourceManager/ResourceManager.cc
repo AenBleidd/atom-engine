465343850a7031425fe11e1aa25548325ed3f9a0
L Lawliet <lestat.de.lionkur@gmail.com>
2011-08-15 16:55:01 +0300

    Fix

--- src/ResourceManager/ResourceManager.cc
+++ src/ResourceManager/ResourceManager.cc
@@ -1,13 +1,7 @@
 #include "ResourceManager.h"
-ResourceManager::ResourceManager(AtomLog *log, AtomFS *fs) {
+ResourceManager::ResourceManager(AtomLog *log) {
   if (log == 0)
     throw -1;
-  if (fs == 0) {
-// TODO(Lawliet): We need to add here some const password
-    try { fs = new AtomFS(log); }
-// TODO(Lawliet): When we will catch this exceptions???
-    catch (int32_t i) { throw -1; }
-  }
   return;
 }
 ResourceManager::~ResourceManager() {
