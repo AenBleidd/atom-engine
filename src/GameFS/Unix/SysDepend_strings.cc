b73825428a0122c95a9fee1ff4e0045c8078f8ef
L Lawliet lestat de lionkur <L Lawliet lestat.de.lionkur@gmail.com>
2012-02-11 23:18:23 +0200

    ??????????? ????? ?????? ? Linux

--- src/GameFS/Unix/SysDepend_strings.cc
+++ src/GameFS/Unix/SysDepend_strings.cc
@@ -1,5 +1,16 @@
+#include <termios.h>
 #include "../strings.h"
 void PassInput(char *input) {
-  fgets(input, 17, stdin);
+  struct termios oldt, newt;
+  tcgetattr( STDIN_FILENO, &oldt );
+  newt = oldt;
+  newt.c_lflag &= ~( ICANON | ECHO );
+  tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+  for (uint8_t i = 0; i < 16; i++)
+    input[i] = getchar(), fprintf(stderr, "*");
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+  input[16] = '\0';
+  fprintf(stderr, "\n");
+
   return;
 }
