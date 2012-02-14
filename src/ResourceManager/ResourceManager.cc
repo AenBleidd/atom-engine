3e5e99f37736a93b52b174aa70b27e2014278e06
L Lawliet <lestat.de.lionkur@gmail.com>
2012-02-14 20:28:04 +0200

    Fix

--- src/ResourceManager/ResourceManager.cc
+++ src/ResourceManager/ResourceManager.cc
@@ -1,9 +1,17 @@
 #include "ResourceManager.h"
-ResourceManager::ResourceManager(AtomLog *log) {
+ResourceManager::ResourceManager(AtomLog *log, RESMAN_PARAM *param) {
   if (log == 0)
     throw -1;
+// allock base memory
+  if (param != 0 && param->start_mem_size != 0)
+    MemBuff = malloc(param->start_mem_size);
+  else
+    MemBuff = malloc(start_mem_block);
   return;
+  
 }
 ResourceManager::~ResourceManager() {
+  if (MemBuff != 0)
+    free(MemBuff);
   return;
 }
