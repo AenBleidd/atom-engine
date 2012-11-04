d8d2dd3ff529b681d0118df7728e8230b5ca5b2c
L Lawliet lestat de lionkur <L Lawliet lestat.de.lionkur@gmail.com>
2012-11-04 14:27:41 +0200

    Fix linux build

--- src/AtomEngine/AtomEngine.h
+++ src/AtomEngine/AtomEngine.h
@@ -18,8 +18,8 @@ public:
   unsigned char prior;
   uint32_t vl_count;
   va_list vl;
-  MESSAGE_QUEUE (OAtomEngine *i_recipient, MODULE_TYPE i_mod_type, 
-                MESSAGE_TYPE i_msg_type, uint32_t i_message, 
+  MESSAGE_QUEUE (OAtomEngine *i_recipient, MODULE_TYPE i_mod_type,
+                MESSAGE_TYPE i_msg_type, uint32_t i_message,
                 unsigned char i_prior, uint32_t i_vl_count, va_list i_vl) {
     recipient = i_recipient;
     mod_type = i_mod_type;
@@ -27,7 +27,8 @@ public:
     message = i_message;
     prior = i_prior;
     vl_count = i_vl_count;
-    vl = i_vl;
+// TODO (Lawliet): Write these correctly because this method is wrong
+//    vl = i_vl;
   }
 
   bool operator < (const MESSAGE_QUEUE &y) const {
@@ -39,10 +40,10 @@ class AtomEngine : public OAtomEngine {
   public:
     AtomEngine();
     ~AtomEngine();
-    int32_t SendMessage(OAtomEngine *recipient, MODULE_TYPE mod_type, 
-                        MESSAGE_TYPE msg_type, uint32_t message, 
+    int32_t SendMessage(OAtomEngine *recipient, MODULE_TYPE mod_type,
+                        MESSAGE_TYPE msg_type, uint32_t message,
                         unsigned char prior, uint32_t vl_count, va_list vl);
-    int32_t GetMessage(OAtomEngine *sender, uint32_t message, 
+    int32_t GetMessage(OAtomEngine *sender, uint32_t message,
                        unsigned char prior, uint32_t vl_count, va_list vl);
     int32_t LoadModule (char *name);
   private:
