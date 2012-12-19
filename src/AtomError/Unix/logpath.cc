484d5ab71e63939db46c7339892af226b084bbca
L Lawliet lestat de lionkur <L Lawliet lestat.de.lionkur@gmail.com>
2012-12-19 18:41:53 +0200

    Fix logpath function

--- src/AtomError/Unix/logpath.cc
+++ src/AtomError/Unix/logpath.cc
@@ -7,11 +7,10 @@ char* AtomLog::GetLogPath(char* temppath, uint32_t size) {
 snprintf(temppath, size, "%s", "log/");
   if (chdir("log") != 0) {
 // TODO(Lawliet): Also we can create log file in the directory /var/log/
-    if (mkdir("log", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0) {
+    if (mkdir("log", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0)
         snprintf(temppath, size, "%s", "/var/log/");
-    } else {
-      chdir("..");
-    }
+  } else {
+    chdir("..");
   }
 #else
   snprintf(temppath, size, "%s", "/var/log/");
