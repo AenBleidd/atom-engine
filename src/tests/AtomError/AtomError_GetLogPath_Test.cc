dc9401a1ddd3dd336a1d60846baf08747becd1db
L Lawliet lestat de lionkur <L Lawliet lestat.de.lionkur@gmail.com>
2012-12-19 18:41:53 +0200

    Fix logpath function

--- src/tests/AtomError/AtomError_GetLogPath_Test.cc
+++ src/tests/AtomError/AtomError_GetLogPath_Test.cc
@@ -5,15 +5,22 @@ TEST(AtomErrorTest, Test_GetLogPath_Correct_CurDir) {
 // TODO(Lawliet): Write here test code for *nix
   char curdir[MAX_PATH];
   char curdir_new[MAX_PATH];
-#ifdef WINDOWS
   EXPECT_NO_THROW({
+#ifdef WINDOWS
     GetCurrentDirectory(MAX_PATH, curdir);
     AtomLog al;
     GetCurrentDirectory(MAX_PATH, curdir_new);
     EXPECT_STREQ(curdir, curdir_new);
-  });
 #endif  // WINDOWS
+#ifdef UNIX
+    getcwd(curdir, MAX_PATH);
+    AtomLog al;
+    getcwd(curdir_new, MAX_PATH);
+    EXPECT_STREQ(curdir, curdir_new);
+#endif  // UNIX
+  });
 }
+
 TEST(AtomErrorTest, Test_GetLogPath) {
   char path[MAX_PATH];
   EXPECT_NO_THROW({
