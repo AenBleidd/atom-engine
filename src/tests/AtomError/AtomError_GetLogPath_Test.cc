a2064cbab5c2fdefed9abf3e67f2214d026ae077
L Lawliet lestat de lionkur <L Lawliet lestat.de.lionkur@gmail.com>
2013-11-25 18:56:03 +0200

    Linux fix

--- src/tests/AtomError/AtomError_GetLogPath_Test.cc
+++ src/tests/AtomError/AtomError_GetLogPath_Test.cc
@@ -3,30 +3,30 @@
 
 TEST(AtomErrorTest, Test_GetLogPath_Correct_CurDir) {
 // TODO(Lawliet): Write here test code for *nix
-  char curdir[MAX_PATH];
-  char curdir_new[MAX_PATH];
+  char curdir[MSG_BUFFER_SIZE];
+  char curdir_new[MSG_BUFFER_SIZE];
 #ifdef WINDOWS
   EXPECT_NO_THROW({
-    GetCurrentDirectory(MAX_PATH, curdir);
+    GetCurrentDirectory(MSG_BUFFER_SIZE, curdir);
     AtomLog al;
-    GetCurrentDirectory(MAX_PATH, curdir_new);
+    GetCurrentDirectory(MSG_BUFFER_SIZE, curdir_new);
     EXPECT_STREQ(curdir, curdir_new);
   });
 #endif  // WINDOWS
 #ifdef UNIX
   EXPECT_NO_THROW({
-    getcwd(curdir, MAX_PATH);
+    getcwd(curdir, MSG_BUFFER_SIZE);
     AtomLog al;
-    getcwd(curdir_new, MAX_PATH);
+    getcwd(curdir_new, MSG_BUFFER_SIZE);
     EXPECT_STREQ(curdir, curdir_new);
   });
 #endif  // UNIX
 }
 
 TEST(AtomErrorTest, Test_GetLogPath) {
-  char path[MAX_PATH];
+  char path[MSG_BUFFER_SIZE];
   EXPECT_NO_THROW({
     AtomLog al;
-    EXPECT_STRNE(0, al.GetLogPath(path, MAX_PATH));
+    EXPECT_STRNE(0, al.GetLogPath(path, MSG_BUFFER_SIZE));
   });
 }
