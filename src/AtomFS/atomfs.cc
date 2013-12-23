8dd027c54a423d1bd0ce517d735a54fdf4fd0e5e
Vitaliy Koshura <lestat.de.lionkur@gmail.com>
2013-12-23 18:35:12 +0200

    Struct reorganize

--- src/AtomFS/atomfs.cc
+++ src/AtomFS/atomfs.cc
@@ -5,5 +5,5 @@ AtomFS::AtomFS(AtomLog *log) {
     throw -1;
   atomlog = log;
   listAtomFSHeader = 0;
-  listAtomFSFiletableRecord = 0;
+  listAtomFSFiletableRecord = new AtomFSFiletableRecord(flag_folder, strlen(root_name), (char*)root_name, root_guid);
 }
